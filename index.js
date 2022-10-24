5b57f2328897df89dc7b518ec72c497a134d02d4
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-10-24 17:36:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// organicFood
+// 4UF8L5T1JOQ47I4m
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://organicFood:<password>@cluster0.qhtylfm.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
